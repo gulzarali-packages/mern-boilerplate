fe2158e5173a7dd25a3b4307578f61e8f1cf46c1
Gulzar Ali <gulzar.ali@renesistech.com>
2023-03-31 12:46:48 +0500

    app.js build 1.0.0

--- dist/app.js
+++ dist/app.js
@@ -8,12 +8,6 @@ const api_1 = __importDefault(require("./routes/api"));
 const app = (0, express_1.default)();
 const port = 3003;
 app.use('/api', api_1.default);
-// async function startServer() {
-//   await mongoose.connect(process.env.APP_DATABASE_URL as any, {
-//     useNewUrlParser: true,
-//   } as any);
-// }
-// startServer();
 app.listen(port, () => {
     return console.log(`Express is listening at http://localhost:${port}`);
 });
