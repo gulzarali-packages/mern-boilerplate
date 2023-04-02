b083bcd92eb6f70c036a35fcefb0003be4df73f5
Gulzar Ali <aligulzar375@gmail.com>
2023-04-02 22:36:27 +0500

    controller stub commenting

--- templates/controller.ts
+++ templates/controller.ts
@@ -1,34 +1,63 @@
 
 
 class templateNameController {
-    async index(req, res){
+    
+    /**
+     * Retrieve a list of records.
+     * @param req The HTTP request object.
+     * @param res The HTTP response object.
+     * @returns Array<object> The list of records.
+     */
+    async index(req, res) {
         try {
             return res.status(200).json('response submitted');
         } catch (error) {
             return res.status(500).json(new Error(error));
         }
     }
-    async show(req, res){
+
+    /**
+     * Retrieve a single record.
+     * @param req The HTTP request object.
+     * @param res The HTTP response object.
+     * @returns Object The single record.
+     */
+    async show(req, res) {
         try {
             return res.status(200).json('response submitted');
         } catch (error) {
             return res.status(500).json(new Error(error));
         }
     }
-    async update(req, res){
+
+    /**
+     * Update a record.
+     * @param req The HTTP request object.
+     * @param res The HTTP response object.
+     * @returns Object The updated record.
+     */
+    async update(req, res) {
         try {
             return res.status(200).json('response submitted');
         } catch (error) {
             return res.status(500).json(new Error(error));
         }
     }
-    async destroy(req, res){
+
+    /**
+     * Delete a record.
+     * @param req The HTTP request object.
+     * @param res The HTTP response object.
+     * @returns Boolean True if the record was successfully deleted, false otherwise.
+     */
+    async destroy(req, res) {
         try {
             return res.status(200).json('response submitted');
         } catch (error) {
             return res.status(500).json(new Error(error));
         }
     }
+
 }
 
 export default new templateNameController();
