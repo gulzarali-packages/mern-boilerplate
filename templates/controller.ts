4368f1776778ed179ff9fe3caf77a069f8738b5e
Gulzar Ali <aligulzar375@gmail.com>
2023-04-02 22:31:22 +0500

    resourcefull controller stub

--- templates/controller.ts
+++ templates/controller.ts
@@ -8,6 +8,27 @@ class templateNameController {
             return res.status(500).json(new Error(error));
         }
     }
+    async show(req, res){
+        try {
+            return res.status(200).json('response submitted');
+        } catch (error) {
+            return res.status(500).json(new Error(error));
+        }
+    }
+    async update(req, res){
+        try {
+            return res.status(200).json('response submitted');
+        } catch (error) {
+            return res.status(500).json(new Error(error));
+        }
+    }
+    async destroy(req, res){
+        try {
+            return res.status(200).json('response submitted');
+        } catch (error) {
+            return res.status(500).json(new Error(error));
+        }
+    }
 }
 
 export default new templateNameController();
